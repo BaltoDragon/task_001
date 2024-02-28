009e7e586fa11e0eac0bd18284f6ef08605750eb
BaltoDragon <a_olegka@bk.ru>
2024-02-28 22:49:11 +0300

    Valid programm code

--- src/roman_to_arabic.c
+++ src/roman_to_arabic.c
@@ -1,11 +1,69 @@
 #include <stdio.h>
 
-#define MAX_LENGTH 15
+#define MAX_LENGTH 16
+
+int roman_to_arabic(char *str);
 
 int main(void) {
-    char str[MAX_LENGTH];
-    if(scanf("%s", str) != 1) printf("Puck you, Verter!");
+    char input[MAX_LENGTH];
+    int output;
 
-    printf("%s", str);
+    fgets(input, sizeof(input), stdin);
+
+    output = roman_to_arabic(input);
+
+    printf("%d", output);
+
     return 0;
+}
+
+int roman_to_arabic(char *str) {
+    int result = 0, i = 0;
+    while (str[i] != '\0') {
+        if (str[i] == 'M') {
+            result += 1000;
+            if (str[i - 1] == 'C') result -= 100;
+        }
+        if (str[i] == 'D') {
+            result += 500;
+            if (str[i + 1] == 'C') {
+                while (str[i + 1] == 'C') {
+                    result += 100;
+                    i++;
+                }
+            }
+            if (str[i - 1] == 'C') result -= 100;
+        }
+        if (str[i] == 'C' && str[i + 1] != 'M' && str[i + 1] != 'D') {
+            result += 100;
+            if (str[i - 1] == 'X') result -= 10;
+        }
+        if (str[i] == 'L') {
+            result += 50;
+            if (str[i + 1] == 'X') {
+                while (str[i + 1] == 'X') {
+                    result += 10;
+                    i++;
+                }
+            }
+            if (str[i - 1] == 'X') result -= 10;
+        }
+        if (str[i] == 'X' && str[i + 1] != 'L' && str[i + 1] != 'C') {
+            result += 10;
+            if (str[i - 1] == 'I') result -= 1;
+        }
+        if (str[i] == 'V') {
+            result += 5;
+            if (str[i + 1] == 'I') {
+                while (str[i + 1] == 'I' && str[i + 1] == '\0') {
+                    result += 1;
+                    i++;
+                }
+            }
+            if (str[i - 1] == 'I') result -= 1;
+        }
+        if (str[i] == 'I' && str[i + 1] != 'V' && str[i + 1] != 'X') result += 1;
+        i++;
+    }
+    return result;
 }
